7fc681cad1b336de93c03304917bc45b616e8590
Will Brennan <willbrennan314@gmail.com>
2021-12-16 08:55:30 -0500

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
 
+    def name=(name)
+        @name = name 
+    end
+
+    def name 
+        @name  
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job 
+    end
+
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
